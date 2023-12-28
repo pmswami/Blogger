0752c3b48b2e5174bbe3a17fe61e2da17fb09580
unknown <usern5949@gmail.com>
2023-12-28 23:44:39 +0530

    added basic components in react

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const title="Welcome to the New Blog"
+  const likes=50
+  // const person = {
+  //   name: "Yoshi", 
+  //   age: 30
+  // }
+  const link = "http://www.google.com/"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        {/* <p>{person}</p> */}
+        <p>{10}</p>
+        <p>{"Hello Ninjas"}</p>
+        <p>{[1,2,3]}</p>
+        <p>{Math.random()*10}</p>
+        <a href={link}>Google Site</a>
+      </div>
     </div>
   );
 }
