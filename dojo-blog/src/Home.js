48f3ebd12e89e5f472e373bbd6912499256b356a
unknown <usern5949@gmail.com>
2023-12-29 16:01:23 +0530

    added handleClick event handlers

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+
+    const handleClick = (event)=>{
+        console.log("Hello Ninjas", event)
+    }
+
+    const handleClickAgain = (name, event)=>{
+        console.log(`Hello ${name}`)
+        console.log(event)
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me!</button>
+            <button onClick={(event) => {handleClickAgain("Yoshi", event)}}>Click Me!</button>
         </div>
     );
 }
