64901373812fe25b5d5752b5268734965ec2a2a0
unknown <usern5949@gmail.com>
2023-12-29 17:07:05 +0530

    added blog preview page including styles

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -3,28 +3,42 @@ import { useState } from "react";
 const Home = () => {
 
     // let name = "Mario";
-    const [name, setName] = useState("Mario")
-    const [age, setAGe] = useState(20)
+    // const [name, setName] = useState("Mario")
+    // const [age, setAGe] = useState(20)
+    const [blogs, setblogs] = useState([
+        {title: "Title 1", body:"Body 1", author: "Author 1", id:1},
+        {title: "Title 2", body:"Body 2", author: "Author 2", id:2},
+        {title: "Title 3", body:"Body 3", author: "Author 3", id:3}
+    ])
 
-    const handleClick = (event)=>{
-        // console.log("Hello Ninjas")
-        // console.log("Hello Ninjas", event)
-        // name="Luigi";
-        // console.log(name)
-        if (name==="Yoshi") setName("Mario")
-        else setName("Yoshi")
-    }
+
+    // const handleClick = (event)=>{
+    //     // console.log("Hello Ninjas")
+    //     // console.log("Hello Ninjas", event)
+    //     // name="Luigi";
+    //     // console.log(name)
+    //     if (name==="Yoshi") setName("Mario")
+    //     else setName("Yoshi")
+    // }
 
     // const handleClickAgain = (name, event)=>{
     //     console.log(`Hello ${name}`)
-    //     // console.log(event)
+    //     // console.log(event) 
     // }
 
     return (
         <div className="home">
-            <h2>Homepage</h2>
+            <div>
+                {blogs.map((blog)=>(
+                    <div className="blog-preview" key={blog.id}>
+                        <h2>{blog.title}</h2>
+                        <p>Written By {blog.author}</p>
+                    </div>
+                ))} 
+            </div>
+            {/* <h2>Homepage</h2>
             <p>{name} is {age} years old.</p>
-            <button onClick={handleClick}>Click Me!</button>
+            <button onClick={handleClick}>Click Me!</button> */}
             {/* <button onClick={(event) => {handleClickAgain("Yoshi", event)}}>Click Me!</button> */}
         </div>
     );
