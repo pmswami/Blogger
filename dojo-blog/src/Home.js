b1390229cb31003914fe4c71ffae2d09f7022c7e
unknown <usern5949@gmail.com>
2023-12-29 16:15:38 +0530

    added useState hook

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,19 +1,31 @@
+import { useState } from "react";
+
 const Home = () => {
 
+    // let name = "Mario";
+    const [name, setName] = useState("Mario")
+    const [age, setAGe] = useState(20)
+
     const handleClick = (event)=>{
-        console.log("Hello Ninjas", event)
+        // console.log("Hello Ninjas")
+        // console.log("Hello Ninjas", event)
+        // name="Luigi";
+        // console.log(name)
+        if (name==="Yoshi") setName("Mario")
+        else setName("Yoshi")
     }
 
-    const handleClickAgain = (name, event)=>{
-        console.log(`Hello ${name}`)
-        console.log(event)
-    }
+    // const handleClickAgain = (name, event)=>{
+    //     console.log(`Hello ${name}`)
+    //     // console.log(event)
+    // }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is {age} years old.</p>
             <button onClick={handleClick}>Click Me!</button>
-            <button onClick={(event) => {handleClickAgain("Yoshi", event)}}>Click Me!</button>
+            {/* <button onClick={(event) => {handleClickAgain("Yoshi", event)}}>Click Me!</button> */}
         </div>
     );
 }
