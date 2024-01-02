f24b55c3c50f6df9db95b908c4922cb74daa187a
unknown <usern5949@gmail.com>
2024-01-02 16:39:38 +0530

    Added React Router and Link

--- dojo-blog/src/Navbar.js
+++ dojo-blog/src/Navbar.js
@@ -1,11 +1,14 @@
 import React from "react"
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>The Dojo Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <Link to="/">Home</Link>
+                <Link to="/create">New Blog</Link>
+                {/* <a href="/">Home</a>
+                <a href="/create">New Blog</a> */}
                 {/* Inline styling
                 <a href="/create" style={{
                     color: "white",
